cf72bf36e1791dda1a5ab189f13df59fa3c5299a
David Barton <barton.david.sv@gmail.com>
2020-07-25 16:16:27 +0200

    Updates

---
+++ precache-manifest.6bc89088a67e74574779864eff60c432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f3a7462ab7477f1d3978bb957a9d6d",
+    "url": "/freedom-of-the-press/index.html"
+  },
+  {
+    "revision": "93fe9ca464d39fd66536",
+    "url": "/freedom-of-the-press/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "11e388cff1d3ec4e42cd",
+    "url": "/freedom-of-the-press/static/css/main.3a94a5d7.chunk.css"
+  },
+  {
+    "revision": "93fe9ca464d39fd66536",
+    "url": "/freedom-of-the-press/static/js/2.5f6c3186.chunk.js"
+  },
+  {
+    "revision": "11e388cff1d3ec4e42cd",
+    "url": "/freedom-of-the-press/static/js/main.f1e63cfc.chunk.js"
+  },
+  {
+    "revision": "a6c641700f63950fc87e",
+    "url": "/freedom-of-the-press/static/js/runtime~main.d2ebdbb8.js"
+  }
+]);
