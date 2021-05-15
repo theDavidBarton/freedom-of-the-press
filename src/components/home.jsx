c12b86c0c63add19e6f09c54ccc1cf53dd87bac5
David Barton <barton.david.sv@gmail.com>
2021-05-15 22:17:46 +0200

    fix: improve repeated text in the bg

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,20 +3,19 @@ import i18n from './../resources/lang_collected.json'
 
 const getTranslation = () => {
   const translationArray = i18n.filter(el => el.code === navigator.language.replace(/-(.*)/, ''))
-  let translation
-  translationArray.length > 0 ? (translation = translationArray[0].translation.toLowerCase()) : (translation = 'freedom')
+  const translation = translationArray.length > 0 ? translationArray[0].translation.toLowerCase() : 'freedom'
   return translation
 }
 
-const getThemAll = () => {
-  const allTranslations = i18n.map(el => el.translation).join(' ')
-  return allTranslations + allTranslations + allTranslations + allTranslations
+const getThemAll = (multiplier) => {
+  const allTranslations = i18n.map(el => el.translation).join(' ') + ' '
+  return allTranslations.repeat(multiplier)
 }
 
 export default function Home() {
   return (
     <div className='position-relative h-100'>
-      <main className='bg-light main bg-text' datacontent={getThemAll()}>
+      <main className='bg-light main bg-text' datacontent={getThemAll(Math.round(window.innerHeight / 100))}>
         <section className='container h-100'>
           <div className='row h-100 justify-content-center align-items-center'>
             <div className='col'>
